89d73a5b1b4c4bbe33ec12dc548347a6bae53678
张建波 <k963@vip.qq.com>
2017-05-08 11:23:06 +0800

    提交

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO user (username,email, password, activated) VALUES ('admin', 'admin@mail.me', 'b8f57d6d6ec0a60dfe2e20182d4615b12e321cad9e2979e0b9f81e0d6eda78ad9b6dcfe53e4e22d1', true);
-INSERT INTO user (username,email, password, activated) VALUES ('user', 'user@mail.me', 'd6dfa9ff45e03b161e7f680f35d90d5ef51d243c2a8285aa7e11247bc2c92acde0c2bb626b1fac74', true);
-INSERT INTO user (username,email, password, activated) VALUES ('rajith', 'rajith@abc.com', 'd6dfa9ff45e03b161e7f680f35d90d5ef51d243c2a8285aa7e11247bc2c92acde0c2bb626b1fac74', true);
+INSERT INTO user (username,email, password, activated) VALUES ('admin', 'admin@mail.me', '08f176fe719d1b1bf18c273ca4770303', true);
+INSERT INTO user (username,email, password, activated) VALUES ('user', 'user@mail.me', 'ede110d2b5e42079f7468db435c339f0', true);
+INSERT INTO user (username,email, password, activated) VALUES ('rajith', 'rajith@abc.com', '28207cdfb01842559aec4e3b1c1f1e8f', true);
 
 INSERT INTO authority (name) VALUES ('ROLE_USER');
 INSERT INTO authority (name) VALUES ('ROLE_ADMIN');
